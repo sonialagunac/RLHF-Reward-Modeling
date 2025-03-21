43e638859edd47fdb6e8febb733d1ecdb2a0ec21
Sonia Laguna <sonia.lagunacillero@inf.ethz.ch>
2025-03-21 14:20:02 +0100

    Sonia job setup

---
+++ bin/stage2prep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=RLHF_interp
+#SBATCH --output=../out_log/%j.out
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=40G
+#SBATCH --time=1-10:00:00
+#SBATCH -p gpu 
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=gpu-biomed-[01,03,07-10,12-16,22]
+
+source ~/.bashrc
+conda deactivate
+conda activate swiss_ai
+
+# --exclude=gpu-biomed-[03,07-10,12-13,22] #to only exc the 2080s
+cd /cluster/home/slaguna/RLHF-Reward-Modeling/armo-rm/
+python stage-2_prepare.py "$@"
