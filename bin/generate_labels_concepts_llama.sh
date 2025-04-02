6a0d1b0de5ac240623470a9e7e13cd0b0498ece2
Sonia Laguna <sonia.lagunacillero@inf.ethz.ch>
2025-04-02 11:33:21 +0200

    temporary scripts to generate llama annotations

--- bin/generate_labels_concepts_llama.sh
+++ bin/generate_labels_concepts_llama.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=../out_log/%j.out
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=20G
-#SBATCH --time=0-05:00:00
+#SBATCH --time=0-07:00:00
 #SBATCH -p gpu 
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=gpu-biomed-[01,03,07-10,12-16,22]
@@ -17,4 +17,4 @@ conda activate swiss_ai
 cd /cluster/home/slaguna/RLHF-Reward-Modeling/armo-rm/prepare_embeddings/
 
 echo "Running batch index $1"
-python api_call.py --batch_index $1 --num_batches $2
+python api_call_llama.py --batch_index $1 --num_batches $2
--- bin/submit_loop.sh
+++ bin/submit_loop.sh
@@ -4,5 +4,5 @@ for i in {0..19}
 # for i in {0..4}
 do
     echo "Submitting batch index $i"
-    sbatch generate_labels_concepts.sh $i 20
+    sbatch generate_labels_concepts_llama.sh $i 20
 done
